f8c944e7efbffcffa87d6f3a82de8529b7ce5421
maxl85 <maxl85@mail.ru>
2023-01-05 01:53:29 +0300

    scss + 07-12

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 // import Image from 'next/image';
 // import { Inter } from '@next/font/google';
-import { Button, Htag } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 // const inter = Inter({ subsets: ['latin'] });
 
@@ -9,8 +9,23 @@ export default function Home(): JSX.Element {
     <>
       <main>
         <Htag tag="h1">Text</Htag>
-        <Button appearance="primary">Button</Button>
-        <Button appearance="ghost">Button</Button>
+        <Button appearance="primary" arrow="right">
+          Button
+        </Button>
+        <Button appearance="ghost" arrow="right">
+          Button
+        </Button>
+        <P size="l">Big</P>
+        <P>Middle</P>
+        <P size="s">Small</P>
+        <Tag size="s">Small</Tag>
+        <Tag size="m" color="red">
+          Red
+        </Tag>
+        <Tag size="s" color="green">
+          Green
+        </Tag>
+        <Tag color="primary">Primary</Tag>
       </main>
     </>
   );
